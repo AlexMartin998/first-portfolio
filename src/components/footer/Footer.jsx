a16dcfed15499dffd88f19f5cfb197d079507b10
Alex Martin <alextravc.342@gmail.com>
2022-08-13 17:48:16 -0500

    [ADD]: footer section added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,62 @@
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+import { SiUdemy } from 'react-icons/si';
+
 import './footer.css';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  let year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        ADRIAN
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/adrian-changalombo-iba"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/AlexMartin998"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.udemy.com/user/adrian-alexander-changalombo-travez-2/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiUdemy />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>Copyright &copy; {year} AC. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
