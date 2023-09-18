75a1c7b241173c02076c2a95431f9a1995d5e607
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-18 11:45:31 -0500

    [ADD]: storebook

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -10,6 +10,7 @@ import IBA_PORTFOLIO from './../../assets/iba-portfolio.png';
 import BOOKING from './../../assets/home-booking.png';
 import HOOBANK from './../../assets/hoobank.png';
 import TESLOSHOP from './../../assets/order-pay.png';
+import STOREBOOK from './../../assets/storebook.png';
 
 const genID = () =>
   (
@@ -17,6 +18,120 @@ const genID = () =>
   ).replace(/\./g, '');
 
 export const data = [
+  {
+    id: genID(),
+    image: TESLOSHOP,
+    title: 'TesloShop E-commerce',
+    tech: 'Next.js | TypeScript | PayPal | Docker',
+    github: 'https://github.com/AlexMartin998/next-teslo-shop',
+    demo: 'https://next-teslo-shop-six.vercel.app/',
+  },
+  {
+    id: genID(),
+    image: UPTASK,
+    title: 'UpTask',
+    tech: 'React.js | Node.js | Socket.IO - MERN Stack',
+    github: 'https://github.com/adrianAlx/uptask-mern-front',
+    demo: 'https://uptask-mern-io-adrianlx.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: ECOMVISION,
+    title: 'ECOMVISION - Dashboard',
+    tech: 'React.js | Node.js | Docker - MERN Stack',
+    github: 'https://github.com/AlexMartin998/admin-dashboard-mui',
+    demo: 'https://ecomvision-reacmuijs-alx.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: STOREBOOK,
+    title: 'Book | Shop',
+    tech: 'Angular | Spring Boot | PayPal | Docker',
+    github: 'https://github.com/AlexMartin998/book-store-angular',
+    demo: 'https://alx-storebook-angular-spring.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: Calendar,
+    title: 'Calendar',
+    tech: 'React.js | Node.js - MERN Stack',
+    github: 'https://github.com/AlexMartin998/calendar-front-react',
+    demo: 'https://calendar-mern-alxs.netlify.app/auth/login',
+  },
+
+  {
+    id: genID(),
+    image: BOOKING,
+    title: 'Booking',
+    tech: 'React.js | Node.js | Docker - MERN Stack',
+    github: 'https://github.com/AlexMartin998/booking-app-react',
+    demo: 'https://booking-app-react-alxadr.netlify.app',
+  },
+  {
+    id: genID(),
+    image: APV,
+    title: 'Veterinary PM',
+    tech: 'React.js | Node.js - MERN Stack',
+    github: 'https://github.com/adrianAlx/apv-mern-frontend',
+    demo: 'https://apv-mern-adrianlx.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: HOOBANK,
+    title: 'Hoobank',
+    tech: 'React.js | TypeScript | TailwindCSS',
+    github: 'https://github.com/AlexMartin998/hoobank-react',
+    demo: 'https://hoobank-reactts-alx.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: ADMIN_PANEL,
+    title: 'Admin Panel',
+    tech: 'React.js | TypeScript | Sass',
+    github: 'https://github.com/adrianAlx/admin-panel-react',
+    demo: 'https://react-admin-panel-bld.netlify.app/',
+  },
+
+  {
+    id: genID(),
+    image: OMNIFOOD,
+    title: 'Omnifood',
+    tech: 'HTML5 | CSS3 | JavaScript',
+    github: 'https://github.com/adrianAlx/omnifood',
+    demo: 'https://omnifood-adrian.netlify.app/',
+  },
+  {
+    id: genID(),
+    image: IBA_PORTFOLIO,
+    title: 'IBA Portfolio',
+    tech: 'React.js',
+    github: 'https://github.com/AlexMartin998/iba-portfolio',
+    demo: 'https://portfolio-carrera-adrian.netlify.app/',
+  },
+  {
+    id: genID(),
+    image: NODE_API,
+    title: 'REST API',
+    tech: 'Node.js | Express.js | TypeScript',
+    github: 'https://github.com/adrianAlx/rest-api-typescript-nodejs',
+    demo: 'https://github.com/adrianAlx/rest-api-typescript-nodejs',
+  },
+  {
+    id: genID(),
+    image: SPRING_BOOT_API,
+    title: 'Inventory Management System',
+    tech: 'Spring Boot | JWT | PostgreSQL',
+    github: 'https://github.com/AlexMartin998/inventory-management-springboot3',
+    demo: 'https://github.com/AlexMartin998/inventory-management-springboot3',
+  },
+];
+
+/* export const data = [
   {
     id: genID(),
     image: TESLOSHOP,
@@ -118,3 +233,4 @@ export const data = [
     demo: 'https://github.com/AlexMartin998/inventory-management-springboot3',
   },
 ];
+ */
