5d6e20f8a89544b49715312c2cb3900c4a1b0108
Adrian Changalombo <alextravc.342@gmail.com>
2023-10-09 22:17:12 -0500

    [ADD]: quill + airbnb (Next.js)

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -1,16 +1,18 @@
 import UPTASK from './../../assets/admin-ut.png';
-import APV from './../../assets/admin.png';
+// import APV from './../../assets/admin.png';
 import Calendar from './../../assets/modal.png';
 import ADMIN_PANEL from './../../assets/home-admin.png';
 import ECOMVISION from './../../assets/dashboard-mui-js.png';
 import OMNIFOOD from './../../assets/omnifood.png';
 import NODE_API from './../../assets/node-api.jpg';
 import SPRING_BOOT_API from './../../assets/spring-boot-api-jwt.png';
-import IBA_PORTFOLIO from './../../assets/iba-portfolio.png';
+// import IBA_PORTFOLIO from './../../assets/iba-portfolio.png';
 import BOOKING from './../../assets/home-booking.png';
 import HOOBANK from './../../assets/hoobank.png';
 import TESLOSHOP from './../../assets/order-pay.png';
 import STOREBOOK from './../../assets/storebook.png';
+import AIRBNB from './../../assets/home-airbnb.png';
+import QUILLPDF from './../../assets/chatpdf-page.png';
 
 const genID = () =>
   (
@@ -28,11 +30,11 @@ export const data = [
   },
   {
     id: genID(),
-    image: UPTASK,
-    title: 'UpTask',
-    tech: 'React.js | Node.js | Socket.IO - MERN Stack',
-    github: 'https://github.com/adrianAlx/uptask-mern-front',
-    demo: 'https://uptask-mern-io-adrianlx.netlify.app/',
+    image: QUILLPDF,
+    title: 'QuillPDF AI',
+    tech: 'Next.js | TypeScript | tRPC | OpenAI',
+    github: 'https://github.com/AlexMartin998/quillpdf-saas-nextjs',
+    demo: 'https://quillpdf-saas-nextjs.vercel.app',
   },
   {
     id: genID(),
@@ -52,6 +54,22 @@ export const data = [
   },
   {
     id: genID(),
+    image: UPTASK,
+    title: 'UpTask',
+    tech: 'React.js | Node.js | Socket.IO - MERN Stack',
+    github: 'https://github.com/adrianAlx/uptask-mern-front',
+    demo: 'https://uptask-mern-io-adrianlx.netlify.app/',
+  },
+  {
+    id: genID(),
+    image: AIRBNB,
+    title: 'Airbnb Clone',
+    tech: 'Next.js | TypeScript | Prisma ORM | Docker',
+    github: 'https://github.com/AlexMartin998/airbnb-clone-nextjs',
+    demo: 'https://adralx-airbnb-nextjs.vercel.app',
+  },
+  {
+    id: genID(),
     image: Calendar,
     title: 'Calendar',
     tech: 'React.js | Node.js - MERN Stack',
@@ -67,14 +85,14 @@ export const data = [
     github: 'https://github.com/AlexMartin998/booking-app-react',
     demo: 'https://booking-app-react-alxadr.netlify.app',
   },
-  {
-    id: genID(),
-    image: APV,
-    title: 'Veterinary PM',
-    tech: 'React.js | Node.js - MERN Stack',
-    github: 'https://github.com/adrianAlx/apv-mern-frontend',
-    demo: 'https://apv-mern-adrianlx.netlify.app/',
-  },
+  // {
+  //   id: genID(),
+  //   image: APV,
+  //   title: 'Veterinary PM',
+  //   tech: 'React.js | Node.js - MERN Stack',
+  //   github: 'https://github.com/adrianAlx/apv-mern-frontend',
+  //   demo: 'https://apv-mern-adrianlx.netlify.app/',
+  // },
 
   {
     id: genID(),
@@ -102,13 +120,21 @@ export const data = [
     github: 'https://github.com/adrianAlx/omnifood',
     demo: 'https://omnifood-adrian.netlify.app/',
   },
+  // {
+  //   id: genID(),
+  //   image: IBA_PORTFOLIO,
+  //   title: 'IBA Portfolio',
+  //   tech: 'React.js',
+  //   github: 'https://github.com/AlexMartin998/iba-portfolio',
+  //   demo: 'https://portfolio-carrera-adrian.netlify.app/',
+  // },
   {
     id: genID(),
-    image: IBA_PORTFOLIO,
-    title: 'IBA Portfolio',
-    tech: 'React.js',
-    github: 'https://github.com/AlexMartin998/iba-portfolio',
-    demo: 'https://portfolio-carrera-adrian.netlify.app/',
+    image: SPRING_BOOT_API,
+    title: 'Inventory Management System',
+    tech: 'Spring Boot | JWT | PostgreSQL',
+    github: 'https://github.com/AlexMartin998/inventory-management-springboot3',
+    demo: 'https://github.com/AlexMartin998/inventory-management-springboot3',
   },
   {
     id: genID(),
@@ -117,14 +143,6 @@ export const data = [
     tech: 'Node.js | Express.js | TypeScript',
     github: 'https://github.com/adrianAlx/rest-api-typescript-nodejs',
     demo: 'https://github.com/adrianAlx/rest-api-typescript-nodejs',
-  },
-  {
-    id: genID(),
-    image: SPRING_BOOT_API,
-    title: 'Inventory Management System',
-    tech: 'Spring Boot | JWT | PostgreSQL',
-    github: 'https://github.com/AlexMartin998/inventory-management-springboot3',
-    demo: 'https://github.com/AlexMartin998/inventory-management-springboot3',
   },
 ];
 
